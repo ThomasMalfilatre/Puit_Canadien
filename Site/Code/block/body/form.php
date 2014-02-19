3defb75d8a59bef60504e5ca68983b8cef24e060
Thomas Malfilatre <thomas.malfilatre@etu.univ-orleans.fr>
2014-02-20 00:54:16 +0100

    ajout des datepicker dans la liste

--- Site/Code/block/body/form.php
+++ Site/Code/block/body/form.php
@@ -7,6 +7,31 @@
 	<br />
 	<br />
 	
-	<div id="baseSondes"></div>
+	<div id="baseSondes"></div> <!-- div qui contiendra la liste des sondes selectionnées -->
+	
+	<br />
+	<br />
+	
+	<script src="scripts/jquery/jquery-1.9.1.js"></script>
+	<script src="scripts/jquery/jquery-ui.js"></script>
+	<label>Date de début</label>
+	<script>
+		$(function() {
+			$( "#datepicker" ).datepicker();
+		});
+	</script>
+	<input type="text" name="datedeb" id="datepicker"/>
+
+	<br />
+	<br />
+
+	<label>Date de fin</label>
+	<br />
+	<script>
+		$(function() {
+			$( "#datepicker" ).datepicker();
+		});
+	</script>
+	<input type="text" name="datefin" id="datepicker"/>
 </form>
 

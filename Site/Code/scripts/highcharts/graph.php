11cefd99f6060f970a1cf400650bd08d3489650b
Thomas Malfilatre <thomas.malfilatre@etu.univ-orleans.fr>
2014-02-27 15:04:57 +0100

    mise en place du graphe (en dur pour l'instant)

--- Site/Code/scripts/highcharts/graph.php
+++ Site/Code/scripts/highcharts/graph.php
@@ -1,4 +1,5 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="http://code.highcharts.com/highcharts.js"></script>
+<script src="graph.js"></script>
 
-<script src="scripts/highcharts/graph.js"></script>
 <div id="container" style="width:100%; height:400px;"></div>
